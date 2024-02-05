770c74d557d7f248b021cbda12c128ef4cd7e975
anm-elbadrawi <anm.elbadrawi@gmail.com>
2024-02-05 02:14:47 -0700

    added accordion

---
+++ project.js
@@ -0,0 +1,8 @@
+function toggleAccordion(element) {
+    var content = element.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  }
